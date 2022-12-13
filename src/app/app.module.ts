c2625cd98973ec5feb0185c04cdfe8cd37b53d5c
DouniaN <nougaouidounia.dev@gmial.com>
2022-12-13 16:37:35 +0100

    Navbar_Mdification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponentComponent } from './user-component/user-component.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponentComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
